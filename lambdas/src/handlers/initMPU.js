109d36e5beee8ccc7d1e508d5505743476538ed2
Lucas Marinho <lucasmarinhodasilva2@gmail.com>
2026-07-09 22:09:49 -0300

    feat: implement multipart upload functionality with signed URLs
    - Enhanced the `initMPU.js` handler to support multipart uploads by adding the ability to generate signed URLs for each part.
    - Introduced `totalChunks` parameter to handle multiple parts during the upload process.
    - Utilized `UploadPartCommand` and `getSignedUrl` from the AWS SDK to facilitate secure uploads.

--- lambdas/src/handlers/initMPU.js
+++ lambdas/src/handlers/initMPU.js
@@ -1,14 +1,16 @@
-import { CreateMultipartUploadCommand, S3Client } from '@aws-sdk/client-s3';
+import { CreateMultipartUploadCommand, S3Client, UploadPartCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { randomUUID } from 'node:crypto';
 
 export const s3 = new S3Client();
 
 export const handler = async (event) => {
-  const { filename } = JSON.parse(event.body);
+  const bucket = 'upload-study';
+  const { filename, totalChunks } = JSON.parse(event.body);
   const filekey = `${randomUUID()}-${filename}`;
 
   const createMPUCommand = new CreateMultipartUploadCommand({
-    Bucket: 'upload-study',
+    Bucket: bucket,
     Key: filekey,
   });
 
@@ -21,6 +23,31 @@ export const handler = async (event) => {
     };
   }
 
-  
-  
+  const signedUrlPromises = [];
+
+  for (let partNumber = 1; partNumber <= totalChunks; partNumber++) {
+    const uploadPartCommand = new UploadPartCommand({
+      Bucket: bucket,
+      Key: filekey,
+      UploadId,
+      PartNumber: 1,
+    });
+
+    const uploadPart = getSignedUrl(
+      s3, uploadPartCommand, { expiresIn: 3600 }
+    );
+
+    signedUrlPromises.push(uploadPart);
+  }
+
+  const urls = await Promise.all(signedUrlPromises);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      filekey,
+      uploadId: UploadId,
+      parts: urls.map((url, index) => ({ url, partNumber: index + 1 })),
+    }),
+  };
 }
